236587dfbfc97834ae12d6818576a37dd1c83d02
maeno kazuki <maeno@MacBookPro13.local>
2014-06-15 03:31:20 +0900

    index.php
    アクセストークン関係

--- php/index.php
+++ php/index.php
@@ -1,18 +1,35 @@
+<?php
+	session_start();
+>
 <html>
 <body>
-phpは動いているのか
 <?php
 	require_once("../facebook/facebook.php");
 	$appdata = array(
 		'appId' => '591353344312115',
 		'secret' => '349bbad20cf514f2f0c1a4df69d17be4'
 	);
-	var_dump($appdata);
 	$facebook = new Facebook($appdata);
-
-	var_dump($appdata);
 	$loginUrl = $facebook->getLoginUrl();
 	echo '<a href="' . $loginUrl . '">Login with Facebook</a>';
+	$userId = $facebook->getLoginUrl();
+
+	$code = $_REQUEST('code');
+	$redirect_uri = 'http://zeglass.cloudapp.net/php/';
+	$token_url = 'https://graph.facebook.com/oauth/access_token'
+					. '?client_id=' . $userId
+					. '&redirect_uri=' . urlencode($redirect_uri)
+					. '&client_secret=' . $appdata['secret'];
+					. '&code=' . $code;
+	// アクセストークン取得
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $token_url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$token = curl_exec($ch);
+
+	echo $token;
+
+
 ?>
 </body>
 <html>
