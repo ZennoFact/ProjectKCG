31dfa2c83401244928a7d19757e5ebae42df82f7
maeno kazuki <maeno@MacBookPro13.local>
2014-06-15 07:15:08 +0900

    index.php
    500

--- php/index.php
+++ php/index.php
@@ -1,42 +1,48 @@
 <?php
 require_once('facebook/facebook.php');
 $config = array(
-	'appId'  => '591353344312115',
-	'secret' => '349bbad20cf514f2f0c1a4df69d17be4'
+        'appId'  => '591353344312115',
+        'secret' => '349bbad20cf514f2f0c1a4df69d17be4'
 );
 var_dump($config);
-$redirect_uri = 'http://zeglass.cloudapp.net/php/'; 
+$redirect_uri = 'http://zeglass.cloudapp.net/php/';
 var_dump($redirect_uri);
 $facebook = new Facebook($config);
- 
-	//ログイン済みの場合はユーザー情報を取得
-	if ($facebook->getUser()) {
-		try {
-			$userId = $facebook->getUser();
-			$user = $facebook->api('/me','GET');
-		} catch(FacebookApiException $e) {
-			error_log($e->getType());
-			error_log($e->getMessage());
-		}
-	}
-}
 
+        //ログイン済みの場合はユーザー情報を取得
+        if ($facebook->getUser()) {
+                try {
+                        $userId = $facebook->getUser();
+                        $user = $facebook->api('/me','GET');
+                } catch(FacebookApiException $e) {
+                        error_log($e->getType());
+                        error_log($e->getMessage());
+                }
+        }
+}
 ?>
 
+<!DOCTYPE html>
+<meta charset="UTF-8">
 <html>
-	<body>
-	<?php
-		if (isset($user)) {
-			//ログイン済みでユーザー情報が取れていれば表示
-			echo '<pre>';
-			echo　$user;
-			echo　$userId;
-			echo '</pre>';
-		} else {
-			//未ログインならログイン URL を取得してリンクを出力
-			$loginUrl = $facebook->getLoginUrl();
-			echo '<a href="' . $loginUrl . '">Login with Facebook</a>';
-		}
-	?>
-	</body>
+<head>
+<title>テスト</title>
+</head>
+        <body>
+        <?php
+                if (isset($user)) {
+                        //ログイン済みでユーザー情報が取れていれば表示
+                        echo '<pre>';
+                        echo　$user;
+                        echo '</pre>';
+                        echo '<pre>';
+                        echo　$userId;
+                        echo '</pre>';
+                } else {
+                        //未ログインならログイン URL を取得してリンクを出力
+                        $loginUrl = $facebook->getLoginUrl();
+                        echo '<a href="' . $loginUrl . '">Login with Facebook</a>';
+                }
+        ?>
+        </body>
 </html>
