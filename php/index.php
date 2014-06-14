ed809764a99a29a36d538fad4be1446161d59cb5
maeno kazuki <maeno@MacBookPro13.local>
2014-06-15 04:08:25 +0900

    index.php
    認機能」

--- php/index.php
+++ php/index.php
@@ -1,65 +1,44 @@
-<?php
-session_start();
->
 <html>
 <body>
 <?php
 require_once('php-sdk/facebook.php');
-$client_secret = '349bbad20cf514f2f0c1a4df69d17be4'; 
+$config = array(
+	'appId'  => '591353344312115',
+	'secret' => '349bbad20cf514f2f0c1a4df69d17be4'
+);
+
 $redirect_uri = 'http://zeglass.cloudapp.net/php/'; 
 
-$code = $_REQUEST['code'];
-$error = $_REQUEST['error'];
-$error_reason = $_REQUEST['error_reason'];
-$error_description = $_REQUEST['error_description'];
+$facebook = new Facebook($config);
+ 
+	//ログイン済みの場合はユーザー情報を取得
+	if ($facebook->getUser()) {
+		try {
 
-if (empty($code) && empty($error)) {
-    $_SESSION['state'] = md5(uniqid(rand(), TRUE));
-        
-    header('Location: https://graph.facebook.com/oauth/authorize'
-                   . '?client_id=' . $client_id
-                   . '&redirect_uri=' . urlencode($redirect_uri)
-                   . '&scope=publish_stream,read_stream'
-                   . '&display=popup'
-                   . '&state=' . $_SESSION['state']);
-} else {
-    if (!empty($code)) {
-        if($_REQUEST['state'] == $_SESSION['state']) { 
-                    $token_url = 'https://graph.facebook.com/oauth/access_token' 
-                       . '?client_id=' . $client_id
-                       . '&redirect_uri=' . urlencode($redirect_uri) 
-                       . '&client_secret=' . $client_secret 
-                       . '&code=' . $code;
-            
-            // アクセストークン取得
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $token_url);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            $token = curl_exec($ch);
-            
-            // アクセストークンを表示
-            echo $token;
-        }
-        
-    } else if (!empty($error)) {
-        
-        // アクセス拒否の場合
-        
-        // Facebookアプリのインストール / 権限許可の画面で
-        // "キャンセル" ボタンを押す等した場合、
-        // "redirect_uri" に指定したURLにGETパラメータ 
-        // "error" "error_reason" "error_description"
-        // が付加されてリダイレクトされる
-        echo 'error:' . $error 
-          . '/error_reason:' . $error_reason 
-          . '/error_description:' . $error_description;
-    }
+			$userId = $facebook->getUser();
+			$user = $facebook->api('/me','GET');
+		} catch(FacebookApiException $e) {
+			error_log($e->getType());
+			error_log($e->getMessage());
+		}
+	}
 }
 
 ?>
 
-
-
-?>
-</body>
 <html>
+	<body>
+	<?php
+		if (isset($user)) {
+			//ログイン済みでユーザー情報が取れていれば表示
+			echo '<pre>';
+			print_r($user);
+			echo '</pre>';
+		} else {
+			//未ログインならログイン URL を取得してリンクを出力
+			$loginUrl = $facebook->getLoginUrl();
+			echo '<a href="' . $loginUrl . '">Login with Facebook</a>';
+		}
+	?>
+	</body>
+</html>
