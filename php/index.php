a6d8d8701fe51a7651a1df275f693f85c5c7a8bf
maeno kazuki <maeno@MacBookPro13.local>
2014-06-15 03:51:23 +0900

    index.php
    oyasuminasai

--- php/index.php
+++ php/index.php
@@ -4,10 +4,8 @@ session_start();
 <html>
 <body>
 <?php
-// ※「アクセストークン取得までの流れ」での解説を参照
-$client_id = '...'; // 【client_id】
-$client_secret = '...'; // 【client_secret】
-$redirect_uri = '...'; // 【redirect_uri】
+$client_secret = '349bbad20cf514f2f0c1a4df69d17be4'; 
+$redirect_uri = 'http://zeglass.cloudapp.net/php/'; 
 
 $code = $_REQUEST['code'];
 $error = $_REQUEST['error'];
@@ -15,19 +13,8 @@ $error_reason = $_REQUEST['error_reason'];
 $error_description = $_REQUEST['error_description'];
 
 if (empty($code) && empty($error)) {
-
-    // Facebook認証後のリダイレクトによるアクセスではない場合
-    // （= GETパラメータに "code" または "error" がない場合）
-    
-    // CSRF protection
     $_SESSION['state'] = md5(uniqid(rand(), TRUE));
         
-    // Facebook認証に遷移
-    // 例；
-    // Facebook未ログインの場合 → Facebookログイン画面を表示
-    // Facebookログイン済 + アプリを許可済の場合
-    // → "redirect_uri" に指定したURLに、GETパラメータ "code" 付きで
-    // リダイレクトされる
     header('Location: https://graph.facebook.com/oauth/authorize'
                    . '?client_id=' . $client_id
                    . '&redirect_uri=' . urlencode($redirect_uri)
@@ -35,19 +22,9 @@ if (empty($code) && empty($error)) {
                    . '&display=popup'
                    . '&state=' . $_SESSION['state']);
 } else {
-    
-    // Facebook認証後のリダイレクトによるアクセスの場合
-    
     if (!empty($code)) {
-        
-        // Facebook認証成功の場合
-        
-        // "redirect_uri" に指定したURLに、
-        // GETパラメータ "code" が付加されてリダイレクトされる
-        if($_REQUEST['state'] == $_SESSION['state']) { // CSRF protection
-            
-            // アクセストークン取得用のURL生成
-            $token_url = 'https://graph.facebook.com/oauth/access_token' 
+        if($_REQUEST['state'] == $_SESSION['state']) { 
+                    $token_url = 'https://graph.facebook.com/oauth/access_token' 
                        . '?client_id=' . $client_id
                        . '&redirect_uri=' . urlencode($redirect_uri) 
                        . '&client_secret=' . $client_secret 
