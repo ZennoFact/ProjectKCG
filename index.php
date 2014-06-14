f09cd52c1df29dbd57c2036eb23b232043a35c9d
maeno kazuki <maeno@MacBookPro13.local>
2014-06-15 00:38:59 +0900

    index.php変更
    php思い出し中。

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+	echo 'テスト';
+	require_once("facebook/facebook.php");
+	$config = array(
+		'appId' => '591353344312115',
+		'secret' => '349bbad20cf514f2f0c1a4df69d17be4'
+	);
+
+	$facebook _ new Facebook($config);
+
+	$loginUrl = $facebook->getLoginUrl();
+	echo '<a href="' . $loginUrl . '">Login with Facebook</a>';
+?>
