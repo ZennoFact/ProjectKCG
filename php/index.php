f32919ca7e90a1f55f4a14e2af44d24b9872c5b2
maeno kazuki <maeno@MacBookPro13.local>
2014-06-15 09:46:59 +0900

    index.php
    ログイン機能チェック

--- php/index.php
+++ php/index.php
@@ -1,47 +1,35 @@
 <?php
-	session_start();
+require 'facebook/facebook.php';
 
-	$client_id = '591353344312115';
-	$client_secret = '349bbad20cf514f2f0c1a4df69d17be4';
-	$redirect_uri = 'http://zeglass.cloudapp.net/php/';
+$config = array(
+        'appId'=>'591353344312115',
+        'secret'=>'349bbad20cf514f2f0c1a4df69d17be4'
+        );
 
-	$code = $_REQUEST['code'];
-	$error = $_REQUEST['error'];
-	$error_reason = $_REQUEST['error_reason'];
-	$error_description = $_REQUEST['error_description'];
-	
-	if (empty($code) && empty(error)) {
-		$_SESSION['state'] = md5(uniqid(rand(), TRUE));
+$facebook = new Facebook( $config );
 
-		header('Location: https//graph.facebook.com/oauth/authorize'
-			. '?client_id' . $client_id
-			. '&redirect_uri' . urlencode($redirect_uri));
-			. '&scope=publish_stream,read_stream'
-			. '&display=popup'
-			. '&state=' . $_SESSION['state']);
-	}
-	else {
-		if (!empty($code)) {
-			if($_REQUEST['state'] == $_SESSION['state']){
-				$token_url = 'https//graph.facebook.com/oauth/authorize'
-				. '?client_id' . $client_id
-				. '&redirect_uri' . urlencode($redirect_uri));
-				. '&scope=publish_stream,read_stream'
-				. '&display=popup'
-				. '&state=' . $_SESSION['state']);
+$user = $facebook->getUser();
+if( ! $user )
+{
+    // Login
+    $loginUrl = $facebook->getLoginUrl();
+    echo "<a href='{$loginUrl}'>Login</a>";
+}
+else
+{
+    try
+    {
+        $userProfile = $facebook->api( '/me' );
+        echo( 'Hello '.$userProfile[ 'name' ] );
+    }
+    catch( FacebookApiException $e )
+    {
+        error_log( $e );
+    }
 
-				$ch = curl_init();
-				curl_setopt($ch, CURLOPT_URL, $token_url);
-				curl_setopt($ch, CURLOPT_RETURHTRANSFER, true);
-				$token =cutl_exec($ch);
+    // Logout
+    $logoutUrl = $facebook->getLogoutUrl();
+    echo "<a href='{$logoutUrl}'>Logout</a>";
+}
 
-				echo $token;
-			}
-		} else if(!empty($error)) {
-			echo "error:" . $error;
-					. '/error_reason:' . $error_reason;
-					.'error_description: . $error_description;
-		}
-	}
-
-?>	
+?>
