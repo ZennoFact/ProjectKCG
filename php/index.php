c720a4da6b16795a40e6258d8b34ca68b91556f1
maeno kazuki <maeno@MacBookPro13.local>
2014-06-15 10:26:18 +0900

    facebook test
    index.php

--- php/index.php
+++ php/index.php
@@ -1,35 +1,80 @@
 <?php
 require 'facebook/facebook.php';
 
-$config = array(
-        'appId'=>'591353344312115',
-        'secret'=>'349bbad20cf514f2f0c1a4df69d17be4'
-        );
-
-$facebook = new Facebook( $config );
+$config = new Facebook(array(
+    'appId'=>'591353344312115',
+    'secret'=>'349bbad20cf514f2f0c1a4df69d17be4'
+));
 
 $user = $facebook->getUser();
-if( ! $user )
-{
-    // Login
-    $loginUrl = $facebook->getLoginUrl();
+if ($user) {
+  try {
+    $user_profile = $facebook->api('/me');
     echo "<a href='{$loginUrl}'>Login</a>";
+  } catch (FacebookApiException $e) {
+    error_log($e);
+    $user = null;
+  }
 }
-else
-{
-    try
-    {
-        $userProfile = $facebook->api( '/me' );
-        echo( 'Hello '.$userProfile[ 'name' ] );
-    }
-    catch( FacebookApiException $e )
-    {
-        error_log( $e );
-    }
 
-    // Logout
+if ($user) {
     $logoutUrl = $facebook->getLogoutUrl();
-    echo "<a href='{$logoutUrl}'>Logout</a>";
+} else {
+    $statusUrl = $facebook->getLoginStatusUrl();
+    $loginUrl = $facebook->getLoginUrl();
 }
 
+$naitik = $facebook->api('/naitik');
 ?>
+
+<!doctype html>
+<html xmlns:fb="http://www.facebook.com/2008/fbml">
+  <head>
+    <title>php-sdk</title>
+    <style>
+      body {
+        font-family: 'Lucida Grande', Verdana, Arial, sans-serif;
+      }
+      h1 a {
+        text-decoration: none;
+        color: #3b5998;
+      }
+      h1 a:hover {
+        text-decoration: underline;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>php-sdk</h1>
+
+    <?php if ($user): ?>
+      <a href="<?php echo $logoutUrl; ?>">Logout</a>
+    <?php else: ?>
+      <div>
+        Check the login status using OAuth 2.0 handled by the PHP SDK:
+        <a href="<?php echo $statusUrl; ?>">Check the login status</a>
+      </div>
+      <div>
+        Login using OAuth 2.0 handled by the PHP SDK:
+        <a href="<?php echo $loginUrl; ?>">Login with Facebook</a>
+      </div>
+    <?php endif ?>
+
+    <h3>PHP Session</h3>
+    <pre><?php print_r($_SESSION); ?></pre>
+
+    <?php if ($user): ?>
+      <h3>You</h3>
+      <img src="https://graph.facebook.com/<?php echo $user; ?>/picture">
+
+      <h3>Your User Object (/me)</h3>
+      <pre><?php print_r($user_profile); ?></pre>
+    <?php else: ?>
+      <strong><em>You are not Connected.</em></strong>
+    <?php endif ?>
+
+    <h3>Public profile of Naitik</h3>
+    <img src="https://graph.facebook.com/naitik/picture">
+    <?php echo $naitik['name']; ?>
+  </body>
+</html>
